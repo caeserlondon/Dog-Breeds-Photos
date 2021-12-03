b9c13668ce3dd136ebe2cd48f8840184ce19d3f9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2021-12-03 15:30:26 +0000

    updating JS file

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let timer;
+let deleteFirstPhotoDelay;
+
 async function start() {
     const response = await fetch("https://dog.ceo/api/breeds/list/all");
     const data = await response.json();
@@ -22,6 +25,48 @@ async function loadByBreed(breed) {
 	if (breed != "Choose a dog breed") {
 		const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
 		const data = await response.json();
-		console.log(data);
+		createSlidShow(data.message);
+	}
+}
+
+function createSlidShow(images) {
+	let currentPosition = 0;
+	clearInterval(timer);
+	clearTimeout(deleteFirstPhotoDelay);
+
+	if (images.length > 1) {
+		document.getElementById("slideshow").innerHTML = `
+	<div class="slide" style="background-image: url('${
+		images[currentPosition]
+	}')"></div>
+	<div class="slide" style="background-image: url('${
+		images[currentPosition + 1]
+	}')"></div>
+	`;
+		currentPosition += 2;
+		if (images.length == 2) currentPosition = 0;
+		timer = setInterval(nextSlide, 3000);
+	} else {
+		document.getElementById("slideshow").innerHTML = `
+	<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>
+	<div class="slide"></div>
+	`;
+	}
+
+	function nextSlide() {
+		document
+			.getElementById("slideshow")
+			.insertAdjacentHTML(
+				"beforeend",
+				`<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`
+			);
+		deleteFirstPhotoDelay = setTimeout(function () {
+			document.querySelector(".slide").remove();
+		}, 1000);
+		if (currentPosition + 1 >= images.length) {
+			currentPosition = 0;
+		} else {
+			currentPosition++;
+		}
 	}
 }
